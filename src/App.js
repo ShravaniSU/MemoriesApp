48e3d3f6816e1ad4861fe3958a6c18142b4a120d
s.shirish.urankar <s.shirish.urankar@accenture.com>
2022-11-19 18:41:53 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
+import memories from "./images/memories.png";
+import Posts from "./components/Posts/Posts.js";
+import Form from "./components/Form/Form.js";
+//import useStyles from "./styles.js";
+import { useDispatch } from "react-redux";
+import { getPosts } from "./actions/posts.js";
+import "./styles.css";
 
 function App() {
+  //let classes = useStyles();
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className="appBar" position="static" color="inherit">
+        <Typography className="heading" variant="h2" align="center">Memories</Typography>
+        <img className="image" src={memories} alt="Memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container className="mainContainer" justifyItems="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
